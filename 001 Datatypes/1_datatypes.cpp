20c15ed61bbb32190b2ad040c7c6a336558ab6f4
= <=>
2023-02-18 18:42:09 +0500

    Learnt Datatypes

---
+++ 001 Datatypes/1_datatypes.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello Google" << endl;
+    int a = 1;
+    char b = 'a';
+    bool c = true;
+    float d = 1.2;
+    double e = 1.5;
+
+    // cout << a << endl;
+    // cout << b << endl;
+    // cout << c << endl;
+    // cout << d << endl;
+    // cout << e << endl;
+    // cout << "Size of a is : " << sizeof(a) << endl;
+
+
+    int z = 'a';
+    cout << z << endl;
+
+    return 0;
+}
