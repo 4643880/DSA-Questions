9b4e6d87317e91b1facd8540c636953dbceab931
= <=>
2023-03-23 23:22:47 +0500

    Solved LeetCode Question Sqrt of x

---
+++ 010 Binary Search Interview Questions Part-2/sqrt.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    int binarySearch(int num){
+        int start = 0;
+        int end = num;
+        long long int mid = start + (end-start)/2;
+        long long int ans = -1;
+        while(start <= end){
+            long long int square = mid * mid;
+            if(square == num){
+                return mid;
+            }else if(square < num){
+                ans = mid;
+                start = mid + 1;
+            }else{
+                end = mid -1;
+            }
+            mid = start + (end-start)/2;
+        }
+        return ans;
+    }
+    int mySqrt(int x) {
+        return binarySearch(x);
+    }
+};
