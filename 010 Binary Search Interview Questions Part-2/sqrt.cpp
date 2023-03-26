627eb4d1052566abb2783f06da2c27d9b7f3e41b
= <=>
2023-03-26 20:44:48 +0500

    More precised find the sqrt in double values

--- 010 Binary Search Interview Questions Part-2/sqrt.cpp
+++ 010 Binary Search Interview Questions Part-2/sqrt.cpp
@@ -1,7 +1,8 @@
-class Solution {
-public:
+#include<iostream>
 
-    int binarySearch(int num){
+using namespace std;
+
+    long long int sqrtInteger(int num){
         int start = 0;
         int end = num;
         long long int mid = start + (end-start)/2;
@@ -20,7 +21,35 @@ public:
         }
         return ans;
     }
-    int mySqrt(int x) {
-        return binarySearch(x);
+
+// n is number e.g 36 or 25 , precision is after . 2 digit or 3 digits 
+double morePrecision(int n, int precision , int tempSolution){
+    double factor = 1;    
+    double ans = tempSolution;
+
+    for(int i = 0; i<precision; i++){
+        factor = factor/10;
+        // 0.1
+        // 0.01
+        // 0.001
+        // ans is 6 or har bar factor plus ho raha ha ans main
+        // (6 ; 6*6 < 37 ; 6+0.1){}
+        for(double j = ans; (j*j)<n ; j=j+factor){
+            ans = j;
+        }
     }
-};
+    return ans;
+}
+
+int main(){
+    int n;
+    cout << "Enter the number..." <<  endl;
+    cin >> n;    
+    int tempSolution = sqrtInteger(n);    
+    cout << "Int Square Root of " << n << " is: " << tempSolution << endl;
+    cout << "Double Square Root of " << n << " is: " << morePrecision(n, 3, tempSolution) << endl;
+    
+    
+
+    return 0;
+}
