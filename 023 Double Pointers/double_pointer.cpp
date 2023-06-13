a7d0c7a9311949b925e1a4d9347176a40c7500a6
Aizaz Haider <aizazhaider720@gmail.com>
2023-06-13 07:33:40 +0500

    Solved some double pointer questions

--- 023 Double Pointers/double_pointer.cpp
+++ 023 Double Pointers/double_pointer.cpp
@@ -5,6 +5,20 @@
 
 using namespace std;
 
+void update(int **ptr2){
+
+
+    // ptr2 = ptr2 + 1;    // Kuch change nhi howa Outside of function because wrong address ko increase kia ha
+    // cout << "alpha: " << ptr2 << endl;
+
+
+    // *ptr2 = *ptr2 + 1; // Change howa ha Outside of function because address ko increase kia ha
+
+    **ptr2 = **ptr2 + 1; // Change howa ha Outside of function because value ko increase kia ha
+
+    
+}
+
 int main(){
 
     int i = 5;
@@ -12,7 +26,7 @@ int main(){
     int *ptr = &i;
 
     int **ptr2 = &ptr;
-
+    /*
     cout << "Ptr pointer have this addresss: " << ptr << endl;
     cout << "Ptr addresss: " << &ptr << endl;
     cout << "Ptr value: " << *ptr << endl;
@@ -26,8 +40,22 @@ int main(){
     cout << &i << endl;
     cout << ptr << endl;
     cout << *ptr2 << endl;
+    */
+
+    cout << "==========================" << endl;
+    cout << *ptr << endl;
+    // Before Update
+    cout << "Before Valeus: " << i << endl;
+    cout << "Before Valeus: " << ptr << endl;
+    cout << "Before Valeus: " << ptr2 << endl;
+
+    update(ptr2);
+    // After Update
+    cout << "After Values: " << i << endl;
+    cout << "After Values: " << ptr << endl;
+    cout << "After Values: " << ptr2 << endl;
+
 
     return 0;
 }
 
-// 20:00
