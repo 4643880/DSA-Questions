b018249e0ca6018c9b8abb3d519f0833f57c4cd4
Aizaz Haider <aizazhaider720@gmail.com>
2024-01-05 00:01:46 +0500

    Solved delete middle element from stack problem

---
+++ 050 Stack Questions Part-1/delete_middle_element_from_stack.cpp
@@ -0,0 +1,40 @@
+
+
+#include <bits/stdc++.h> 
+
+// |10|
+// |8|
+// |6|
+// |4|
+// |2|
+// Need to remove mid => 6
+
+// Step 1: base case
+// Step 2: store top element then pop it
+// Step 3: recursive call with ++count or count+1, count++ will not work
+// Step 4: push back the top element again into the stack
+
+void solve(stack<int>&inputStack,int count, int size){
+   int mid = size/2;
+
+   // Base Case
+   if(count == mid){
+      inputStack.pop();
+      return;
+   }
+
+   // Storing the top element
+   int topNum = inputStack.top();
+   inputStack.pop();
+
+   // Recursive Call
+   solve(inputStack, ++count, size);
+
+   // Pushing again top element
+   inputStack.push(topNum);
+}
+void deleteMiddle(stack<int>&inputStack, int N){
+   
+   int count = 0;
+   solve(inputStack, count, N);
+}
